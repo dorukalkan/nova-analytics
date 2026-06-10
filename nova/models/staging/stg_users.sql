c4476d69bbf3fdd88effb00a6f7e3b4c6f939327
yasemendundar <yasemennursalim@gmail.com>
2026-06-10 12:16:26 +0300

    markets,users,services cleaned

--- nova/models/staging/stg_users.sql
+++ nova/models/staging/stg_users.sql
@@ -1,2 +1,20 @@
+with source as (
 select *
 from {{ source('raw', 'nova_users') }}
+),
+cleaned as (
+select
+User_ID as user_id,
+Join_Date as join_date,
+Membership_Tier as membership_tier,
+Primary_Device as primary_device,
+Market_ID as market_id,
+Market_Name as market_name,
+Region as region,
+Acquisition_Source as acquisition_source,
+User_Segment as user_segment,
+Lifecycle_Segment as lifecycle_segment
+from source
+)
+
+select * from cleaned
