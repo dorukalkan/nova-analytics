0302ad58755e250fac37fc6ac688f93403d6e90b
yasemendundar <yasemennursalim@gmail.com>
2026-06-10 12:50:02 +0300

    tab added

--- nova/models/staging/stg_markets.sql
+++ nova/models/staging/stg_markets.sql
@@ -4,14 +4,14 @@ from {{ source('raw', 'nova_markets') }}
 ),
 cleaned as (
 select
-Market_ID as market_id,
-Market_Name as market_name,
-Region as region,
-Latitude as latitude,
-Longitude as longitude,
-Market_Weight as market_weight,
-Growth_Multiplier as growth_multiplier,
-IOS_Share as ios_share
+    Market_ID as market_id,
+    Market_Name as market_name,
+    Region as region,
+    Latitude as latitude,
+    Longitude as longitude,
+    Market_Weight as market_weight,
+    Growth_Multiplier as growth_multiplier,
+    IOS_Share as ios_share
 from source
 )
 
