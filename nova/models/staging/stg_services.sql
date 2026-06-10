af97d4e4a7d2e3d21c73db9481e73c8da06a1a7d
yasemendundar <yasemennursalim@gmail.com>
2026-06-10 12:50:02 +0300

    tab added

--- nova/models/staging/stg_services.sql
+++ nova/models/staging/stg_services.sql
@@ -4,15 +4,15 @@ from {{ source('raw', 'nova_services') }}
 ),
 cleaned as (
 select
-Service_ID as service_id,
-Merchant_Name as merchant_name,
-Category as category,
-Rating as rating,
-Market_ID as market_id,
-Market_Name as market_name,
-Region as region,
-Service_Tier as service_tier,
-Popularity_Score as popularity_score
+    Service_ID as service_id,
+    Merchant_Name as merchant_name,
+    Category as category,
+    Rating as rating,
+    Market_ID as market_id,
+    Market_Name as market_name,
+    Region as region,
+    Service_Tier as service_tier,
+    Popularity_Score as popularity_score
 from source
 )
 
